935f47537a736a7b4077259137cf513fd4ba0a4b
Joel Goh Chong Hui <147377225+joelgoh15@users.noreply.github.com>
2026-06-24 20:35:43 +0800

    upload initial project files.

---
+++ classlibrary_console_web_proj1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classlibrary_console_web_proj1
+{
+    public static class Class1
+    {
+        public static string GetMessage()
+        {
+            return "test message 123";
+        
+        }
+    }
+}
